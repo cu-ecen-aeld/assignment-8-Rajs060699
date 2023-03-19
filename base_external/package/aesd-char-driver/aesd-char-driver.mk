a47e9772f97d92296ae520cae3ece45dcc97fb97
Rajs060699 <rasr7993@colorado.edu>
2023-03-19 02:35:29 +0000

    Master:Assignment 8 P-2

---
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -0,0 +1,16 @@
+
+AESD_CHAR_DRIVER_VERSION =7f70e8fb63d50b24197f9261b0c930fb5d2fe27e
+AESD_CHAR_DRIVER_SITE =git@github.com:cu-ecen-aeld/assignments-3-and-later-Rajs060699.git
+AESD_CHAR_DRIVER_SITE_METHOD = git
+AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver/
+
+define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin	
+	
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
